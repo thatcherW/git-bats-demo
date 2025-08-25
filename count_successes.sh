05734d0a1127b9037f54b277227c1dab3e3e8675
Thatcher Werner <werne662@morris.umn.edu>
2025-08-25 18:57:01 -0500

    Added proper functionality to `count_successes.sh`
    Added the counting to `count_successes.sh`, and ensured the temp files
    are removed once it is finished.
    
    Tests pass without issue, but when running manually there are a few
    issues. Unsure of exact reasoning.

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
-#
-echo "Hello, world!"
+
+tar_file=$1
+
+SCRATCH=$(mktemp -d)
+
+tar -zxf "$tar_file" --directory="$SCRATCH"
+
+num_successes=$(grep -lr SUCCESS "$SCRATCH" | wc -l)
+num_failures=$(grep -lr FAILURE "$SCRATCH" | wc -l)
+
+echo "There were $num_successes successes and $num_failures failures."
+
+rm -rf "$SCRATCH"
